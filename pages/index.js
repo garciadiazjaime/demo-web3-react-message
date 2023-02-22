a1db3bfa9ead7502d8a13f7c80b67c0d93dff098
jaime <garciadiazjaime@gmail.com>
2023-02-21 21:26:51 -0600

    feat: verify sign message

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { ethers } from "ethers";
 import { recoverTypedSignature } from "@metamask/eth-sig-util";
@@ -9,10 +11,12 @@ import abi from "../utils/SignedMessage.json";
 
 import styles from "../styles/Home.module.css";
 
-const contractAddress = "0x8DA24A0C0D224FB7BC947853E3B8E66cd3177461";
 const contractABI = abi.abi;
 
 const sendMessage = async () => {
+  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  console.log({ accounts });
+
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
   console.log({ provider, signer });
@@ -30,84 +34,160 @@ const sendMessage = async () => {
   console.log("Mined -- ", txn.hash);
 };
 
+const signMessageV4 = async () => {
+  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  console.log({ accounts });
+
+  const msgParams = JSON.stringify({
+    domain: {
+      chainId: 5,
+      name: "Message Example",
+      verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+      version: "1",
+    },
+    message: {
+      contents: "Hello, Bob!",
+    },
+    primaryType: "Message",
+    types: {
+      EIP712Domain: [
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "chainId", type: "uint256" },
+        { name: "verifyingContract", type: "address" },
+      ],
+      Message: [{ name: "contents", type: "string" }],
+    },
+  });
+
+  const from = accounts[0];
+
+  const params = [from, msgParams];
+  const method = "eth_signTypedData_v4";
+
+  web3.currentProvider.sendAsync(
+    {
+      method,
+      params,
+      from,
+    },
+    async function (err, result) {
+      if (err) return console.dir(err);
+      if (result.error) {
+        alert(result.error.message);
+      }
+      if (result.error) return console.error("ERROR", result);
+      console.log({ result });
+      console.log("TYPED SIGNED:" + JSON.stringify(result.result));
+
+      const recovered = recoverTypedSignature({
+        data: JSON.parse(msgParams),
+        signature: result.result,
+        version: "V4",
+      });
+
+      const signature = result.result.substring(2);
+      const r = "0x" + signature.substring(0, 64);
+      const s = "0x" + signature.substring(64, 128);
+      const v = parseInt(signature.substring(128, 130), 16);
+      console.log({ signature, r, s, v });
+
+      if (toChecksumAddress(recovered) === toChecksumAddress(from)) {
+        console.log("Successfully recovered signer as " + from);
+      } else {
+        console.log(
+          "Failed to verify signer when comparing " + result + " to " + from
+        );
+      }
+    }
+  );
+};
+
+const verifyMessage = async () => {
+  const msg1 = "heeeyyyy!";
+  const num1 = 46;
+  const msgParams = [
+    {
+      type: "string",
+      name: "Message",
+      value: msg1,
+    },
+    {
+      type: "uint256",
+      name: "num",
+      value: num1,
+    },
+  ];
+
+  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  console.log({ accounts });
+  const from = accounts[0];
+
+  const params = [msgParams, from];
+  const method = "eth_signTypedData";
+  web3.currentProvider.sendAsync(
+    {
+      method,
+      params,
+      from,
+    },
+    async function (err, result) {
+      if (err) return console.dir(err);
+      if (result.error) {
+        alert(result.error.message);
+      }
+      let sign = result.result;
+      console.log("EthSignTyped SIGNED:" + JSON.stringify(sign));
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      console.log({ provider, signer });
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      console.log({ sign, msg1, num1, from });
+
+      const response = await contract.verifyAddressFromTypedSign(
+        sign,
+        msg1,
+        num1,
+        from
+      );
+      console.log({ response });
+
+      const msg = await contract.getMessage();
+      console.log({ msg });
+    }
+  );
+};
+
 export default function Home() {
-  console.log(ethers);
+  const [contract, setContract] = useState();
 
   const clickHandler = async () => {
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    console.log({ accounts });
-
-    const flag = "send_message";
+    // const flag = "send_message";
+    const flag = "verify_message";
 
     if (flag === "send_message") {
-      await sendMessage().catch(error => console.log(error));
+      await sendMessage().catch((error) => console.log(error));
 
       return;
     }
 
-    const msgParams = JSON.stringify({
-      domain: {
-        chainId: 5,
-        name: "Message Example",
-        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
-        version: "1",
-      },
-      message: {
-        contents: "Hello, Bob!",
-      },
-      primaryType: "Message",
-      types: {
-        EIP712Domain: [
-          { name: "name", type: "string" },
-          { name: "version", type: "string" },
-          { name: "chainId", type: "uint256" },
-          { name: "verifyingContract", type: "address" },
-        ],
-        Message: [{ name: "contents", type: "string" }],
-      },
-    });
+    if (flag === "verify_message") {
+      await verifyMessage().catch((error) => console.log(error));
 
-    const from = accounts[0];
+      return;
+    }
 
-    var params = [from, msgParams];
-    var method = "eth_signTypedData_v4";
+    if (flag === "sign_message_v4") {
+      await signMessageV4();
 
-    web3.currentProvider.sendAsync(
-      {
-        method,
-        params,
-        from,
-      },
-      function (err, result) {
-        if (err) return console.dir(err);
-        if (result.error) {
-          alert(result.error.message);
-        }
-        if (result.error) return console.error("ERROR", result);
-        console.log({ result });
-        console.log("TYPED SIGNED:" + JSON.stringify(result.result));
-
-        const recovered = recoverTypedSignature({
-          data: JSON.parse(msgParams),
-          signature: result.result,
-          version: "V4",
-        });
-
-        const signature = result.result.substring(2);
-        const r = "0x" + signature.substring(0, 64);
-        const s = "0x" + signature.substring(64, 128);
-        const v = parseInt(signature.substring(128, 130), 16);
-        console.log({ signature, r, s, v });
-
-        if (toChecksumAddress(recovered) === toChecksumAddress(from)) {
-          console.log("Successfully recovered signer as " + from);
-        } else {
-          console.log(
-            "Failed to verify signer when comparing " + result + " to " + from
-          );
-        }
-      }
-    );
+      return;
+    }
   };
 
   const onNewMessage = (from, timestamp, message) => {
@@ -116,22 +196,28 @@ export default function Home() {
 
   const init = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
+
     const signer = await provider.getSigner();
-
+    console.log({ signer });
     const contract = new ethers.Contract(contractAddress, contractABI, signer);
-    contract.on("NewMessage", onNewMessage);
 
-    return () => {
-      contract.off("NewWave", onNewWave);
-    };
+    setContract(contract);
   };
 
   useEffect(() => {
-    const unload = init();
+    if (!contract) {
+      return;
+    }
 
+    contract.on("NewMessage", onNewMessage);
     return () => {
-      unload();
+      console.log('out cool!')
+      contract.off("NewMessage", onNewMessage);
     };
+  }, [contract]);
+
+  useEffect(() => {
+    init();
   }, []);
 
   return (
