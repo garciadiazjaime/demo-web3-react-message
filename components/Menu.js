a310660ef888c54ec471b5ffa9822c8da142dc38
jaime <garciadiazjaime@gmail.com>
2023-03-06 08:27:07 -0600

    feat: menu active

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./Menu.module.css";
 
 export default function Menu() {
+  const router = useRouter();
+
   return (
     <nav className={styles.menu}>
-      <Link href="/">Home</Link>
-      <Link href="/activity">Activity</Link>
+      <Link href="/" className={router.pathname == "/" ? styles.active : ""}>Home</Link>
+      <Link href="/activity" className={router.pathname == "/activity" ? styles.active : ""}>Activity</Link>
     </nav>
   );
 }
